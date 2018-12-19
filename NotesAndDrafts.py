3f891a52a9e7d96c530001af08d4268149425904
Jeri <35116106+irej98@users.noreply.github.com>
2018-12-19 01:40:58 +0000

    Initial tests and trials to investigate behaviour of lists within numpy arrays in python.
    More investigation to be done.

---
+++ NotesAndDrafts.py
@@ -0,0 +1,8 @@
+# creating a 9 by 9 array which contains list with possibilities for each position
+# arr = np.ndarray(shape = (9,9,1), dtype=np.ndarray)
+
+# adding a posibility
+# arr[0,0,0].append(4)
+
+# removing a possibility from a position
+# arr[0,0,0].remove(4)
