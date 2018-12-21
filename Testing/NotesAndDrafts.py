248c0a2129f4de70dc753b983f36ac5e5a29983d
Jeri <35116106+irej98@users.noreply.github.com>
2018-12-21 03:34:02 +0000

    Update NotesAndDrafts.py

--- Testing/NotesAndDrafts.py
+++ Testing/NotesAndDrafts.py
@@ -1,8 +1,14 @@
 # creating a 9 by 9 array which contains list with possibilities for each position
-# arr = np.ndarray(shape = (9,9,1), dtype=np.ndarray)
+arr = np.ndarray(shape = (9,9,1), dtype=np.ndarray)
 
 # adding a posibility
-# arr[0,0,0].append(4)
+arr[0,0,0].append(4)
 
 # removing a possibility from a position
-# arr[0,0,0].remove(4)
+arr[0,0,0].remove(4)
+
+# error catching if removing a possibility was never there:
+try:
+    arr.remove(10)
+except ValueError:
+    pass  # do nothing!
